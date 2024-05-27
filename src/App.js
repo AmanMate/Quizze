49d0c6f87d0bfa326b8fb08ce55f5f3cb5782074
Aman Mate <aman26mate@gmail.com>
2024-05-27 17:31:11 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import QuestionwiseAnalysis from "./pages/analytics/QuestionwiseAnalysis";
+
+function App() {
+  return (
+    <div className="App">
+      <QuestionwiseAnalysis></QuestionwiseAnalysis>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/createQuiz/CreateQuiz.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+import "./CreateQuiz.css";
+
+export default function CreateQuiz() {
+  return (
+    <div>
+      <div class="navbar">
+        <Navbar></Navbar>
+      </div>
+      <div class="page">dfsdfs</div>
+    </div>
+  );
+}
