d5ff9a946a2ca6ef47bd5405cb59f4c9f4f3e6c2
Aman Mate <aman26mate@gmail.com>
2024-05-28 20:06:06 +0530

    Login Page Created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Analytics from "./pages/analytics/Analytics";
+import CreateQuiz from "./pages/createQuiz/CreateQuiz";
 import QuestionwiseAnalysis from "./pages/analytics/QuestionwiseAnalysis";
+import Login from "./pages/login/Login";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <QuestionwiseAnalysis></QuestionwiseAnalysis>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/analytics" element={<Analytics />} />
+        <Route path="/create-quiz" element={<CreateQuiz />} />
+        <Route
+          path="/questionwiseanalysis"
+          element={<QuestionwiseAnalysis />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,78 +1,140 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios"
+import { useNavigate, Link, Form } from "react-router-dom";
 import "./Login.css";
 
 export default function Login() {
-  const [activeForm, setActiveForm] = useState("logIn");
+  const [activeForm, setActiveForm] = useState("signUp");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setconfirmPassword] = useState("");
 
-  const signUpClick = () => {
-    setActiveForm("signUp");
-  };
+  
+  async function signUpClick(e){
+    e.preventDefault();
+
+    try{
+
+      await axios("http://localhost:4000/user/save",{
+        action: " ",
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data: JSON.stringify({
+          name: name,
+          email: email,
+          password: password,
+          confirmpassword: password
+        })
+      })
+    }
+    catch(e){
+      console.log(e);
+    }
+  }
+
+  async function logInClick(e){
+    e.preventDefault();
+
+    try{
+
+      await axios("http://localhost:4000/user/find",{
+        action: " ",
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data: JSON.stringify({
+          email: email,
+          password: password,
+        })
+      })
+    }
+    catch(e){
+      console.log(e);
+    }
+  }
+
 
   return (
     <div>
-      <div className="slides">
-        <div class="slidesHead">
-          <div className="inline">
-            <button
-              id="signUpButton"
-              onClick={signUpClick}
-              class={activeForm === "signUp" ? "activeButton" : ""}
-            >
-              Sign Up
-            </button>
-          </div>
-          <div className="inline">
-            <button
-              id="logInButton"
-              onClick={() => setActiveForm("logIn")}
-              class={activeForm === "logIn" ? "activeButton" : ""}
-            >
-              Log In
-            </button>
-          </div>
-        </div>
+      <div class="container">
+        <div id="quizzieBox">
+          <h1>Quizzie</h1>
 
-        <div class="slidesBody">
-          {activeForm === "logIn" && (
-            <div class="logIn inline">
-              <div>
-                <p class="inline emailin">Email</p>
-                <input class="inline"></input>
+          <div className="slides">
+            <div class="slidesHead">
+              <div className="inline">
+                <button
+                  id="signUpButton"
+                  onClick={signUpClick}
+                  class={activeForm === "signUp" ? "activeButton" : ""}
+                >
+                  Sign Up
+                </button>
               </div>
-              <div>
-                <p class="inline passin">Password</p>
-                <input class="inline"></input>
-              </div>
-              <div>
-                <button className="blueButton">log In</button>
-              </div>
-            </div>
-          )}
-          {activeForm === "signUp" && (
-            <div class="inline signUp">
-              <div>
-                <p class="inline name">Name</p>
-                <input class="inline"></input>
-              </div>
-              <div>
-                <p class="inline email">Email</p>
-                <input class="inline"></input>
-              </div>
-              <div>
-                <p class="inline pass">Password</p>
-                <input class="inline"></input>
-              </div>
-              <div>
-                <p class="inline confpass">Confirm Password</p>
-                <input class="inline"></input>
-              </div>
-              <div>
-                <button className="blueButton">Sign Up</button>
+              <div className="inline">
+                <button
+                  id="logInButton"
+                  onClick={() => setActiveForm("logIn")}
+                  class={activeForm === "logIn" ? "activeButton" : ""}
+                >
+                  Log In
+                </button>
               </div>
             </div>
-          )}
+
+            <div class="slidesBody">
+              {activeForm === "logIn" && (
+                <div class="logIn inline">
+                  <div>
+                    <p class="inline emailin">Email</p>
+                     <input type="email" value={email} onChange={(e) => { setEmail(e.target.value)}}></input>
+                  </div>
+                  <div>
+                    <p class="inline passin">Password</p>
+                     <input type="password" value={password} onChange={(e) => { setPassword(e.target.value)}}></input>
+                  </div>
+                  <div>
+                    <Link to="/dashboard">
+                      <button type="submit" className="blueButton" onClick={(e) => logInClick(e)}>log In</button>
+                    </Link>
+                  </div>
+                </div>
+              )}
+              {activeForm === "signUp" && (
+                <div class="inline signUp">
+                  <div>
+                    <p class="inline name">Name</p>
+                    <input type="name" value={name} onChange={(e) => { setName(e.target.value)}}></input>
+                  </div>
+                  <div>
+                    <p class="inline email">Email</p>
+                    <input type="email" value={email} onChange={(e) => { setEmail(e.target.value)}}></input>
+                  </div>
+                  <div>
+                    <p class="inline pass">Password</p>
+                    <input type="password" value={password} onChange={(e) => { setPassword(e.target.value)}}></input>
+                  </div>
+                  <div>
+                    <p class="inline confpass">Confirm Password</p>
+                    <input type="password" value={confirmpassword} onChange={(e) => { setconfirmPassword(e.target.value)}}></input>
+                  </div>
+                  <div>
+                    <button type="submit" className="blueButton" onClick={(e) => signUpClick(e)}>Sign Up</button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
 }
+
+
+
+
