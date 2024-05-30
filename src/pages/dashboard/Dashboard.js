32b374db3506793159edbe202ab43f65778ba6ff
Aman Mate <aman26mate@gmail.com>
2024-05-30 22:56:36 +0530

    first commit

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -9,65 +9,59 @@ const Dashboard = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-      const fetchData = async () => {
-          try {
-              const response = await axios.get('http://127.0.0.1:4000/dashboard/getDetails?user_email=a@gmail.com');
-              console.log(response.data)
-<<<<<<< HEAD
-              setData(response.data);
-=======
-              setData(response);
->>>>>>> 1cb34f9a77217e1a6737a5ece13555d4ea1ccd4d
-          } catch (error) {
-            setError(error);
-          } finally {
-            setLoading(false);
-        }
-      };
-      fetchData();
-  }, []); // Empty dependency array means this useEffect runs once when the component mounts.
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:4000/dashboard/getDetails?user_email=a@gmail.com');
+        console.log(response.data);
+        setData(response.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
 
   if (loading) {
     return <div>Loading...</div>;
-}
+  }
 
-if (error) {
+  if (error) {
     return <div>Error: {error.message}</div>;
-}
+  }
 
-return (
+  return (
     <div>
-      <Navbar></Navbar>
-      <div class="page inline">
-        <div class="highligths">
-          <div class="inline quiz">
-            <h2 >
+      <Navbar />
+      <div className="page inline">
+        <div className="highligths">
+          <div className="inline quiz">
+            <h2>
               <span id="spanId">{data.quizCreated}</span>Quiz Created
             </h2>
-           
           </div>
-          <div class="inline ques">
+          <div className="inline ques">
             <h2>
               <span>{data.questionsCreated}</span>Questions Created
             </h2>
           </div>
-          <div class="inline imp">
+          <div className="inline imp">
             <h2>
               <span>{data.totalImpression}</span>Total Imprresions
             </h2>
           </div>
         </div>
         <div>
-          
-          <h2 class="trending">Trending Quizs</h2>
-          <div class="grid-container container">
-            {data && data.trendingQuiz && data.trendingQuiz.map((data, index) => (
-              <div class="grid-item" key={index}>
+          <h2 className="trending">Trending Quizs</h2>
+          <div className="grid-container container">
+            {data && data.trendingQuiz && data.trendingQuiz.map((quiz, index) => (
+              <div className="grid-item" key={index}>
                 <p>
-                  <span class="heading">{data.quiz_name}</span>
-                  <span class="views">{data.impression_count}</span>
+                  <span className="heading">{quiz.quiz_name}</span>
+                  <span className="views">{quiz.impression_count}</span>
                 </p>
-                <p class="created-on">{data.created_date}</p>
+                <p className="created-on">{quiz.created_date}</p>
               </div>
             ))}
           </div>
@@ -75,10 +69,6 @@ return (
       </div>
     </div>
   );
-}
+};
 
-<<<<<<< HEAD
 export default Dashboard;
-=======
-export default Dashboard;
->>>>>>> 1cb34f9a77217e1a6737a5ece13555d4ea1ccd4d
